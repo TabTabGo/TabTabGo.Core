3d7cddb3c0fa66d2994aa3a149a075cfd2fbfd16
Ghassan Samara <gmsamara@gmail.com>
2022-08-26 15:38:48 +0400

    fix: unit of work name get repo fix and add support to not inject IGeneraicRepo

--- src/core/TabTabGo.Core/Extensions/CollectionExtenstions.cs
+++ src/core/TabTabGo.Core/Extensions/CollectionExtenstions.cs
@@ -16,13 +16,19 @@ namespace TabTabGo.Core.Extensions
 
         public static ICollection<TResult> Foreach<T, TResult>(this ICollection<T> collection, Func<T, TResult> func)
         {
-
             var resultList = new List<TResult>();
             foreach (var item in collection)
             {
                 resultList.Add(func(item));
             }
+
             return resultList;
+        }
+
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (T obj in items)
+                collection.Add(obj);
         }
     }
 }
