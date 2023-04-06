d6b17f1a21529c10433a8f3ed1621659ac290e73
Ghassan Samara <ghassan@tabtabgo.com>
2023-04-06 10:40:41 +0400

    feat: add ILocalizationService

---
+++ src/core/TabTabGo.Core/Services/ILocalizationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace TabTabGo.Core.Services;
+public interface ILocalizationService
+{
+    string GetCurrentLanguage();
+    CultureInfo GetCurrentCulture();
+    string GetLanguageByCultureCode(string cultureCode);
+}
